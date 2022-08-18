acb071c2ab69d58476bb0f3b7b66556d0e01b89d
washington gama <wgamaneto16@gmail.com>
2022-08-18 15:00:01 -0300

    requisito 1

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system">oi</div>
+      <>
+        <div data-testid="solar-system">oi</div>
+        <Title />
+      </>
     );
   }
 }
