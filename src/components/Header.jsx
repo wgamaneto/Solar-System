ec20a2bed799383b73e04e95e678f5197cfceb3a
washington gama <wgamaneto16@gmail.com>
2022-08-18 15:00:55 -0300

    requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return (<header>Sistema Solar</header>);
+    return (
+      <header>
+        <h1>Sistema Solar</h1>
+      </header>);
   }
 }
 
