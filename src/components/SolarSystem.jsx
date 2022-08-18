fc1aea054a0def1fc91cb43d333ecdc840ad569d
washington gama <wgamaneto16@gmail.com>
2022-08-18 15:27:46 -0300

    requisito 6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <>
-        <div data-testid="solar-system">oi</div>
+      <div data-testid="solar-system">
         <Title headline="Planetas" />
-      </>
+        {
+          planets.map(({ name, image }) => (
+            <PlanetCard key={ name } planetName={ name } planetImage={ image } />
+          ))
+        }
+      </div>
     );
   }
 }
